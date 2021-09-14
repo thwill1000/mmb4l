13f600122d2badfe96dbe39c78d917ba6befdd18
Thomas Hugo Williams <thomas.hugo.williams@gmail.com>
2021-09-14 20:39:42 +0100

    Fix INPUT$() to not block if no keyboard input

--- linux/linux-src/functions/fun_inputstr.c
+++ linux/linux-src/functions/fun_inputstr.c
@@ -1,22 +1,33 @@
+#include "../common/console.h"
+#include "../common/error.h"
 #include "../common/file.h"
+#include "../common/version.h"
 
 void fun_inputstr(void) {
-    int nbr, fnbr;
-    char *p;
     getargs(&ep, 3, ",");
-    if (argc != 3) error("Invalid syntax");
-    nbr = getinteger(argv[0]);
-    if (nbr < 1 || nbr > MAXSTRLEN) error("Number out of bounds");
+    if (argc != 3) ERROR_SYNTAX;
+    int nbr = getint(argv[0], 1, MAXSTRLEN);
     if (*argv[2] == '#') argv[2]++;
-    fnbr = getinteger(argv[2]);
     sret = GetTempStrMemory();  // this will last for the life of the command
-    p = sret + 1;               // point to the start of the char array
-    *sret = nbr;                // set the length of the returned string
-    while (nbr) {
-        if (MMfeof(fnbr)) break;
-        *p++ = MMfgetc(fnbr);  // get the char and save in our returned string
-        nbr--;
-    }
-    *sret -= nbr;  // correct if we get less than nbr chars
     targ = T_STR;
+    int fnbr = getinteger(argv[2]);
+
+    if (fnbr == 0) {  // accessing the console
+        int i;
+        for (i = 1; i <= nbr && console_kbhit(); i++) {
+            sret[i] = getConsole();  // get the char from the console input
+                                     // buffer and save in our returned string
+        }
+        *sret = i - 1;
+    } else {
+        char *p = sret + 1;  // point to the start of the char array
+        *sret = nbr;         // set the length of the returned string
+        while (nbr) {
+            if (MMfeof(fnbr)) break;
+            *p++ =
+                MMfgetc(fnbr);  // get the char and save in our returned string
+            nbr--;
+        }
+        *sret -= nbr;  // correct if we get less than nbr chars
+    }
 }
