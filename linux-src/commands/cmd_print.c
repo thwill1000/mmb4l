ccf27e207188d46f01c3309717cfeb5c8f4a84e9
Thomas Hugo Williams <thomas.hugo.williams@gmail.com>
2021-11-12 22:30:29 +0000

    Refactor 'cmd_print.c'
    Reduce number of places we are actually printing and introduce
    locali PRINT_MMSTRING macro so they can easily be changed.

--- linux-src/commands/cmd_print.c
+++ linux-src/commands/cmd_print.c
@@ -1,4 +1,9 @@
+#include <stdbool.h>
+
+#include "../common/error.h"
 #include "../common/version.h"
+
+#define PRINT_MMSTRING(s, fnbr)  MMfputs(s, fnbr)
 
 void cmd_print(void) {
 
@@ -6,11 +11,9 @@ void cmd_print(void) {
     MMFLOAT f;
     long long int i64;
     int i, t, fnbr;
-    int docrlf;                                                     // this is used to suppress the cr/lf if needed
+    bool docrlf = true;                                             // this is used to suppress the cr/lf if needed
 
     getargs(&cmdline, (MAX_ARG_COUNT * 2) - 1, ";,");               // this is a macro and must be the first executable stmt
-
-    docrlf = true;
 
     if (argc > 0 && *argv[0] == '#') {                              // check if the first arg is a file number
         argv[0]++;
@@ -22,9 +25,9 @@ void cmd_print(void) {
         i = 0;
     }
 
-    for (; i < argc; i++) {                                          // step through the arguments
+    for (; i < argc; i++) {                                         // step through the arguments
         if (*argv[i] == ',') {
-            MMfputc('\t', fnbr);                                    // print a tab for a comma
+            PRINT_MMSTRING("\1\t", fnbr);                           // print a tab for a comma
             docrlf = false;                                         // a trailing comma should suppress CR/LF
         }
         else if (*argv[i] == ';') {
@@ -32,23 +35,28 @@ void cmd_print(void) {
         }
         else {                                                      // we have a normal expression
             p = argv[i];
-            while(*p) {
+            while (*p) {
                 t = T_NOTYPE;
                 p = evaluate(p, &f, &i64, &s, &t, true);            // get the value and type of the argument
                 if (t & T_NBR) {
                     *inpbuf = ' ';                                  // preload a space
-                    FloatToStr(inpbuf + ((f >= 0) ? 1:0), f, 0, STR_AUTO_PRECISION, ' ');// if positive output a space instead of the sign
-                    MMfputs(CtoM(inpbuf), fnbr);                    // convert to a MMBasic string and output
+                    FloatToStr(inpbuf + ((f >= 0) ? 1:0), f, 0, STR_AUTO_PRECISION, ' '); // if positive output a space instead of the sign
+                    s = CtoM(inpbuf);                               // convert to a MMBasic string
                 } else if (t & T_INT) {
                     *inpbuf = ' ';                                  // preload a space
                     IntToStr(inpbuf + ((i64 >= 0) ? 1:0), i64, 10); // if positive output a space instead of the sign
-                    MMfputs(CtoM(inpbuf), fnbr);                    // convert to a MMBasic string and output
+                    s = CtoM(inpbuf);                               // convert to a MMBasic string
                 } else if (t & T_STR) {
-                    MMfputs(s, fnbr);                               // print if a string (s is a MMBasic string)
+                    // Do nothing, 's' is already the MMBasic string we wish to output.
+                } else {
+                    ERROR_INTERNAL_FAULT;
                 }
+
+                PRINT_MMSTRING(s, fnbr);
             }
             docrlf = true;
         }
     }
-    if (docrlf) MMfputs("\2\r\n", fnbr);                            // print the terminating cr/lf unless it has been suppressed
+
+    if (docrlf) PRINT_MMSTRING("\2\r\n", fnbr);                     // print the terminating cr/lf unless it has been suppressed
 }
