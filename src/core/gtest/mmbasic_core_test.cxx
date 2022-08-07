adc86dbded09fcfa4652da750cd42105a0b2d877
Thomas Hugo Williams <thomas.hugo.williams@gmail.com>
2022-08-07 16:11:47 +0100

    Stubs to unit-test MMBasic.c

--- src/core/gtest/mmbasic_core_test.cxx
+++ src/core/gtest/mmbasic_core_test.cxx
@@ -9,17 +9,62 @@
 extern "C" {
 
 #include "../../Hardware_Includes.h"
+#include "../MMBasic.h"
+#include "../Commands.h"
 #include "../FunTable.h"
 #include "../mmbasic_core_xtra.h"
 
 char error_msg[256];
 
+void CheckAbort(void) { }
+
+void cmd_read_clear_cache()  { }
+
+/** Write a NULL terminated stirng to the console. */
+void console_puts(const char *s) {
+}
+
+void error_init(ErrorState *error_state) { }
+
+void error_throw_ex(MmResult error, const char *msg, ...) {
+    strcpy(error_msg, msg);
+}
+
 void error_throw_legacy(const char *msg, ...) {
     strcpy(error_msg, msg);
 }
 
-struct s_funtbl funtbl[MAXSUBFUN] = { 0 };
-const char *subfun[MAXSUBFUN] = { 0 };
+void file_close_all(void) { }
+
+bool interrupt_check(void) {
+    return false;
+}
+
+void interrupt_clear(void) { }
+
+// Declared in MMBasic.c
+extern struct s_funtbl funtbl[MAXSUBFUN];
+
+// Declared in "main.c"
+char *CFunctionFlash;
+char *CFunctionLibrary;
+char **FontTable;
+ErrorState *mmb_error_state_ptr = &mmb_normal_error_state;
+Options mmb_options;
+ErrorState mmb_normal_error_state;
+
+// Declared in "Commands.c"
+char DimUsed;
+int doindex;
+struct s_dostack dostack[MAXDOLOOPS];
+const char *errorstack[MAXGOSUB];
+int forindex;
+struct s_forstack forstack[MAXFORLOOPS + 1];
+int gosubindex;
+const char *gosubstack[MAXGOSUB];
+int TraceBuffIndex;
+const char *TraceBuff[TRACE_BUFF_SIZE];
+int TraceOn;
 
 } // extern "C"
 
@@ -168,3 +213,177 @@ TEST_F(MmBasicCoreTest, FunctionTableFind) {
     EXPECT_EQ(-1, mmb_function_table_find("name_33_characters_zzzzzzzzzzzzzz"));
     EXPECT_STREQ("SUB/FUNCTION name too long", error_msg);
 }
+
+extern "C" {
+
+void cmd_autosave() { }
+void cmd_call() { }
+void cmd_case() { }
+void cmd_cfunction() { }
+void cmd_chdir() { }
+void cmd_clear() { }
+void cmd_close() { }
+void cmd_cls() { }
+void cmd_console() { }
+void cmd_const() { }
+void cmd_continue() { }
+void cmd_copy() { }
+void cmd_cursor() { }
+void cmd_dim() { }
+void cmd_do() { }
+void cmd_dummy() { }
+void cmd_edit() { }
+void cmd_else() { }
+void cmd_end() { }
+void cmd_endfun() { }
+void cmd_erase() { }
+void cmd_error() { }
+void cmd_execute() { }
+void cmd_exit() { }
+void cmd_exitfor() { }
+void cmd_files() { }
+void cmd_for() { }
+void cmd_gosub() { }
+void cmd_goto() { }
+void cmd_if() { }
+void cmd_inc() { }
+void cmd_input() { }
+void cmd_ireturn() { }
+void cmd_kill() { }
+void cmd_let() { }
+void cmd_lineinput() { }
+void cmd_list() { }
+void cmd_load() { }
+void cmd_loop() { }
+void cmd_longstring() { }
+void cmd_math() { }
+void cmd_memory() { }
+void cmd_mid() { }
+void cmd_mkdir() { }
+void cmd_new() { }
+void cmd_next() { }
+void cmd_null() { }
+void cmd_on() { }
+void cmd_open() { }
+void cmd_option() { }
+void cmd_pause() { }
+void cmd_poke() { }
+void cmd_print() { }
+void cmd_quit() { }
+void cmd_randomize() { }
+void cmd_read() { }
+void cmd_rename() { }
+void cmd_restore() { }
+void cmd_return() { }
+void cmd_rmdir() { }
+void cmd_run() { }
+void cmd_seek() { }
+void cmd_select() { }
+void cmd_settick() { }
+void cmd_settitle() { }
+void cmd_sort() { }
+void cmd_subfun() { }
+void cmd_system() { }
+void cmd_timer() { }
+void cmd_trace() { }
+void cmd_troff() { }
+void cmd_tron() { }
+void cmd_xmodem() { }
+void fun_abs() { }
+void fun_acos() { }
+void fun_asc() { }
+void fun_asin() { }
+void fun_at() { }
+void fun_atn() { }
+void fun_bin() { }
+void fun_bound() { }
+void fun_call() { }
+void fun_choice() { }
+void fun_chr() { }
+void fun_cint() { }
+void fun_cos() { }
+void fun_cwd() { }
+void fun_date() { }
+void fun_datetime() { }
+void fun_day() { }
+void fun_deg() { }
+void fun_dir() { }
+void fun_eof() { }
+void fun_epoch() { }
+void fun_errmsg() { }
+void fun_errno() { }
+void fun_eval() { }
+void fun_exp() { }
+void fun_field() { }
+void fun_fix() { }
+void fun_format() { }
+void fun_hex() { }
+void fun_hres() { }
+void fun_inkey() { }
+void fun_inputstr() { }
+void fun_instr() { }
+void fun_int() { }
+void fun_json() { }
+void fun_lcase() { }
+void fun_lcompare() { }
+void fun_left() { }
+void fun_len() { }
+void fun_lgetbyte() { }
+void fun_lgetstr() { }
+void fun_linstr() { }
+void fun_llen() { }
+void fun_loc() { }
+void fun_lof() { }
+void fun_log() { }
+void fun_math() { }
+void fun_max() { }
+void fun_mid() { }
+void fun_min() { }
+void fun_mmcmdline() { }
+void fun_mmdevice() { }
+void fun_mminfo() { }
+void fun_oct() { }
+void fun_peek() { }
+void fun_pi() { }
+void fun_pos() { }
+void fun_rad() { }
+void fun_rgb() { }
+void fun_right() { }
+void fun_rnd() { }
+void fun_sgn() { }
+void fun_sin() { }
+void fun_space() { }
+void fun_sqr() { }
+void fun_str() { }
+void fun_string() { }
+void fun_tab() { }
+void fun_tan() { }
+void fun_time() { }
+void fun_timer() { }
+void fun_ucase() { }
+void fun_val() { }
+void fun_version() { }
+void fun_vres() { }
+void op_add() { }
+void op_and() { }
+void op_div() { }
+void op_divint() { }
+void op_equal() { }
+void op_exp() { }
+void op_gt() { }
+void op_gte() { }
+void op_inv() { }
+void op_invalid() { }
+void op_lt() { }
+void op_lte() { }
+void op_mod() { }
+void op_mul() { }
+void op_ne() { }
+void op_not() { }
+void op_or() { }
+void op_shiftleft() { }
+void op_shiftright() { }
+void op_subtract() { }
+void op_xor() { }
+
+} // extern "C"
