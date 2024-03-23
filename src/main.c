413bde203b149bcbc90e203b6a3d841ee085c958
Thomas Hugo Williams <thomas.hugo.williams@gmail.com>
2024-03-23 15:27:29 +0000

    Extract MMgetline() into 'mmgetline.c'
    So that it can be used in unit-tests without having to include 'main.c'.

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@ MMBasic for Linux (MMB4L)
 
 main.c
 
-Copyright 2021-2023 Geoff Graham, Peter Mather and Thomas Hugo Williams.
+Copyright 2021-2024 Geoff Graham, Peter Mather and Thomas Hugo Williams.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
@@ -440,88 +440,6 @@ int MMgetchar(void) {
     }
     prevchar = c;
     return c == '\n' ? '\r' : c;
-}
-
-// get a line from the keyboard or a file handle
-void MMgetline(int filenbr, char *p) {
-    int c, nbrchars = 0;
-    const char *tp;
-
-    while (1) {
-        CheckAbort();  // jump right out if CTRL-C
-
-        if ((file_table[filenbr].type == fet_file) && file_eof(filenbr)) break; // End of file.
-        c = file_getc(filenbr);
-
-        // -1 - no character.
-        //  0 - the null character which we ignore.
-        if (c <= 0) continue;
-
-        // if this is the console, check for a programmed function key and
-        // insert the text
-        if (filenbr == 0) {
-            tp = NULL;
-            if (c == F2) tp = "RUN";
-            if (c == F3) tp = "LIST";
-            if (c == F4) tp = "EDIT";
-            if (c == F5) tp = "WEDIT";
-            if (tp) {
-                strcpy(p, tp);
-                console_puts(tp);
-                console_puts("\r\n");
-                return;
-            }
-        }
-
-        if (c == '\t') {  // expand tabs to spaces
-            do {
-                if (++nbrchars > MAXSTRLEN) ERROR_LINE_TOO_LONG;
-                *p++ = ' ';
-                if (filenbr == 0) console_putc(' ');
-            } while (nbrchars % mmb_options.tab);
-            continue;
-        }
-
-        if (c == '\b') {  // handle the backspace
-            if (nbrchars) {
-                if (filenbr == 0) console_puts("\b \b");
-                nbrchars--;
-                p--;
-            }
-            continue;
-        }
-
-        if (c == '\n') {  // what to do with a newline
-            break;        // a newline terminates a line (for a file or serial)
-        }
-
-        if (c == '\r') {
-            if (filenbr == 0) {
-                console_puts("\r\n");
-                break;  // on the console this means the end of the line
-                        // - stop collecting
-            } else {
-                continue;  // for files and serial loop around looking for the
-                           // following newline
-            }
-        }
-
-        if (isprint(c) && (filenbr == 0)) {
-            console_putc(c);  // The console requires that chars be echoed
-        }
-
-        if (++nbrchars > MAXSTRLEN) ERROR_LINE_TOO_LONG;  // stop collecting if maximum length
-
-        // TODO: currently this function can return strings containing control
-        //       characters, i.e. c < 32.
-        //       Perhaps we should replace these with another character such as
-        //       '?' or with a hex code <02> or <0x02>.
-        //       The same might apply to c = 0 which we currently ignore.
-        //       Possibly the behaviour could be controlled by an OPTION.
-
-        *p++ = c;  // save our char
-    }
-    *p = 0;
 }
 
 // dump a memory area to the console
