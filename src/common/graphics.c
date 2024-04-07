70132e33c80b4413f45aa0c74a09cf70964bae0f
Thomas Hugo Williams <thomas.hugo.williams@gmail.com>
2024-04-07 16:43:43 +0100

    Implement interim key handling when graphics window has focus

--- src/common/graphics.c
+++ src/common/graphics.c
@@ -42,6 +42,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 *******************************************************************************/
 
+#include "console.h"
 #include "cstring.h"
 #include "error.h"
 #include "events.h"
