c59a6ba9df38aee0a3dfd14a08e150f76680b92b
Thomas Hugo Williams <thomas.hugo.williams@gmail.com>
2024-05-02 22:30:26 +0100

    Add parse_fn_sig() to parse FUNCTION/SUB signatures

--- src/common/mmresult.h
+++ src/common/mmresult.h
@@ -118,6 +118,15 @@ typedef enum {
     kInvalidFlag,
     kUnexpectedText,
     kCannotBlitCloseWindow,
+    kMissingType,
+    kTypeSpecifiedTwice,
+    kInvalidFunctionDefinition,
+    kInvalidSubDefinition,
+    kMissingCloseBracket,
+    kMissingOpenBracket,
+    kUnexpectedCloseBracket,
+    kInvalidArrayParameter,
+    kTooManyParameters,
 } MmResultCode;
 
 /**
