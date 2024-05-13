47433c8326c778583a6296914149d73b53a7587f
Thomas Hugo Williams <thomas.hugo.williams@gmail.com>
2024-05-13 09:26:50 +0100

    Destroy all Graphics surfaces on END

--- src/commands/cmd_end.c
+++ src/commands/cmd_end.c
@@ -44,9 +44,11 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "../common/mmb4l.h"
 #include "../common/exit_codes.h"
+#include "../common/graphics.h"
 
 void cmd_end(void) {
     getargs(&cmdline, 1, ",");
+    graphics_term();  // To destroy all graphics surface including open windows.
     mmb_exit_code = (argc == 1) ? getint(argv[0], 0, 255) : EX_OK;
     longjmp(mark, JMP_END);
 }
