02b5b362cd9e621c3f73c7fbcc5cf5922747f310
Thomas Hugo Williams <thomas.hugo.williams@gmail.com>
2024-09-17 21:37:05 +0100

    When opening a .BMP, .PNG or .SPR try different cases
     ... for file extension if it is unspecified.

--- src/common/path.c
+++ src/common/path.c
@@ -83,8 +83,7 @@ bool path_is_regular(const char *path) {
     return (stat(path, &st) == 0) && S_ISREG(st.st_mode) ? true : false;
 }
 
-bool path_has_suffix(
-        const char *path, const char *suffix, bool case_insensitive) {
+bool path_has_suffix(const char *path, const char *suffix, bool case_insensitive) {
     int start = strlen(path) - strlen(suffix);
     if (start < 0) return 0;
     for (size_t i = 0; i < strlen(suffix); ++i) {
@@ -535,4 +534,35 @@ MmResult path_complete(const char *path, char *out, size_t sz) {
     }
 
     return kOk;
+}
+
+MmResult path_try_extension(const char *path, const char *extension, char *out, size_t out_sz) {
+    if (extension[0] != '.') return kFileInvalidExtension;
+
+    // Check for an exact match.
+    if (path_exists(path) && path_has_suffix(path, extension, true)) {
+        if (SUCCEEDED(cstring_cpy(out, path, out_sz))) {
+            return kOk;
+        } else {
+            return kFilenameTooLong;
+        }
+    }
+
+    // Try various capitalisations of the extension.
+    char ext[3][32];
+    if (FAILED(cstring_cpy(ext[0], extension, 32))) return kStringTooLong;
+    cstring_tolower(ext[0]); // All lower-case
+    if (FAILED(cstring_cpy(ext[1], extension, 32))) return kStringTooLong;
+    cstring_tolower(ext[1]);
+    ext[1][1] = toupper(ext[1][1]); // Capital letter immediately after the period,
+                                    // remainder lower-case.
+    if (FAILED(cstring_cpy(ext[2], extension, 32))) return kStringTooLong;
+    cstring_toupper(ext[2]); // All upper-case.
+    for (int i = 0; i < 3; ++i) {
+        if (FAILED(cstring_cpy(out, path, out_sz))) return kFilenameTooLong;
+        if (FAILED(cstring_cat(out, ext[i], out_sz))) return kFilenameTooLong;
+        if (path_exists(out)) return kOk;
+    }
+
+    return kFileNotFound;
 }
