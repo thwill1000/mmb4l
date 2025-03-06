1464ebe70614727b9eca86b992f5e79ae37c4a95
Thomas Hugo Williams <thomas.hugo.williams@gmail.com>
2025-03-06 09:23:02 +0000

    refactor: extract cmd_const() into 'commands/cmd_const.c'

---
+++ src/commands/cmd_const.c
@@ -0,0 +1,75 @@
+/*-*****************************************************************************
+
+MMBasic for Linux (MMB4L)
+
+cmd_dim.c
+
+Copyright 2021-2025 Geoff Graham, Peter Mather and Thomas Hugo Williams.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holders nor the names of its contributors
+   may be used to endorse or promote products derived from this software
+   without specific prior written permission.
+
+4. The name MMBasic be used when referring to the interpreter in any
+   documentation and promotional material and the original copyright message
+   be displayed  on the console at startup (additional copyright messages may
+   be added).
+
+5. All advertising materials mentioning features or use of this software must
+   display the following acknowledgement: This product includes software
+   developed by Geoff Graham, Peter Mather and Thomas Hugo Williams.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*******************************************************************************/
+
+#include "../common/mmb4l.h"
+#include "../core/tokentbl.h"
+
+void cmd_const(void) {
+    const char *p;
+    void *v;
+    int i, type;
+
+    getargs(&cmdline, (MAX_ARG_COUNT * 2) - 1, ",");                // getargs macro must be the first executable stmt in a block
+    if((argc & 0x01) == 0) ERROR_SYNTAX;
+
+    for(i = 0; i < argc; i += 2) {
+        p = skipvar(argv[i], false);                                // point to after the variable
+        skipspace(p);
+        if(tokenfunction(*p) != op_equal) ERROR_SYNTAX;             // must be followed by an equals sign
+        p++;                                                        // step over the equals sign
+        type = T_NOTYPE;
+        v = DoExpression(p, &type);                                 // evaluate the constant's value
+        type = TypeMask(type);
+        type |= V_FIND | V_DIM_VAR | T_CONST | T_IMPLIED;
+        if(LocalIndex != 0) type |= V_LOCAL;                        // local if defined in a sub/fun
+        findvar(argv[i], type);                                     // create the variable
+        if(vartbl[VarIndex].dims[0] != 0) error_throw_legacy("Invalid constant");
+        if(TypeMask(vartbl[VarIndex].type) != TypeMask(type)) error_throw_legacy("Invalid constant");
+        else {
+            if(type & T_NBR) vartbl[VarIndex].val.f = *(MMFLOAT *)v; // and set its value
+            if(type & T_INT) vartbl[VarIndex].val.i = *(MMINTEGER *)v;
+            if(type & T_STR) Mstrcpy(vartbl[VarIndex].val.s, (char *)v);
+        }
+    }
+}
