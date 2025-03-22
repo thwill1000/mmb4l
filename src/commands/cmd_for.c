206e9ed952830759193e3825cdfc448c5704a761
Thomas Hugo Williams <thomas.hugo.williams@gmail.com>
2025-03-22 19:06:30 +0000

    refactor: extract cmd_for() into 'commands/cmd_for.c'

---
+++ src/commands/cmd_for.c
@@ -0,0 +1,157 @@
+/*-*****************************************************************************
+
+MMBasic for Linux (MMB4L)
+
+cmd_for.c
+
+Copyright 2021-2025 Geoff Graham, Peter Mather and Thomas Hugo Williams.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holders nor the names of its contributors
+   may be used to endorse or promote products derived from this software
+   without specific prior written permission.
+
+4. The name MMBasic be used when referring to the interpreter in any
+   documentation and promotional material and the original copyright message
+   be displayed  on the console at startup (additional copyright messages may
+   be added).
+
+5. All advertising materials mentioning features or use of this software must
+   display the following acknowledgement: This product includes software
+   developed by Geoff Graham, Peter Mather and Thomas Hugo Williams.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*******************************************************************************/
+
+#include <string.h>
+
+#include "../common/mmb4l.h"
+#include "../core/Commands.h"
+#include "../core/tokentbl.h"
+
+void cmd_for(void) {
+    int i, t, vlen, test;
+    char ss[4];                                                     // this will be used to split up the argument line
+    const char *p, *tp, *xp;
+    void *vptr;
+    char *vname, vtype;
+
+    ss[0] = tokenEQUAL;
+    ss[1] = tokenTO;
+    ss[2] = tokenSTEP;
+    ss[3] = 0;
+
+    {                                                               // start a new block
+        getargs(&cmdline, 7, ss);                                   // getargs macro must be the first executable stmt in a block
+        if(argc < 5 || argc == 6 || *argv[1] != ss[0] || *argv[3] != ss[1]) error_throw_legacy("FOR with misplaced = or TO");
+        if(argc == 6 || (argc == 7 && *argv[5] != ss[2])) ERROR_SYNTAX;
+
+        // get the variable name and trim any spaces
+        vname = argv[0];
+        if(*vname && *vname == ' ') vname++;
+        while(*vname && vname[strlen(vname) - 1] == ' ') vname[strlen(vname) - 1] = 0;
+        vlen = strlen(vname);
+        vptr = findvar(argv[0], V_FIND);                            // create the variable
+        if(vartbl[VarIndex].type & T_CONST) error_throw_legacy("Cannot change a constant");
+        vtype = TypeMask(vartbl[VarIndex].type);
+        if(vtype & T_STR) error_throw_legacy("Invalid variable");   // sanity check
+
+        // check if the FOR variable is already in the stack and remove it if it is
+        // this is necessary as the program can jump out of the loop without hitting
+        // the NEXT statement and this will eventually result in a stack overflow
+        for(i = 0; i < forindex ;i++) {
+            if(forstack[i].var == vptr && forstack[i].level == LocalIndex) {
+                while(i < forindex - 1) {
+                    forstack[i].forptr = forstack[i+1].forptr;
+                    forstack[i].nextptr = forstack[i+1].nextptr;
+                    forstack[i].var = forstack[i+1].var;
+                    forstack[i].vartype = forstack[i+1].vartype;
+                    forstack[i].level = forstack[i+1].level;
+                    forstack[i].tovalue.i = forstack[i+1].tovalue.i;
+                    forstack[i].stepvalue.i = forstack[i+1].stepvalue.i;
+                    i++;
+                }
+                forindex--;
+                break;
+            }
+        }
+
+        if(forindex == MAXFORLOOPS) error_throw_legacy("Too many nested FOR loops");
+
+        forstack[forindex].var = vptr;                              // save the variable index
+        forstack[forindex].vartype = vtype;                         // save the type of the variable
+        forstack[forindex].level = LocalIndex;                      // save the level of the variable in terms of sub/funs
+        forindex++;                                                 // incase functions use for loops
+        if(vtype & T_NBR) {
+            *(MMFLOAT *)vptr = getnumber(argv[2]);                  // get the starting value for a float and save
+            forstack[forindex - 1].tovalue.f = getnumber(argv[4]);  // get the to value and save
+            if(argc == 7)
+                forstack[forindex - 1].stepvalue.f = getnumber(argv[6]);// get the step value for a float and save
+            else
+                forstack[forindex - 1].stepvalue.f = 1.0;           // default is +1
+        } else {
+            *(MMINTEGER *)vptr = getinteger(argv[2]);               // get the starting value for an integer and save
+            forstack[forindex - 1].tovalue.i = getinteger(argv[4]); // get the to value and save
+            if(argc == 7)
+                forstack[forindex - 1].stepvalue.i = getinteger(argv[6]);// get the step value for an integer and save
+            else
+                forstack[forindex - 1].stepvalue.i = 1;             // default is +1
+        }
+        forindex--;
+
+        forstack[forindex].forptr = nextstmt + 1;                   // return to here when looping
+
+        // now find the matching NEXT command
+        t = 1; p = nextstmt;
+        while(1) {
+            p = GetNextCommand(p, &tp, "No matching NEXT");
+            const CommandToken cmd = commandtbl_decode(p);
+            if (cmd == cmdFOR) t++;                                 // count the FOR
+            if (cmd == cmdNEXT) {                                   // is it NEXT
+                xp = p + sizeof(CommandToken);                      // point to after the NEXT token
+                while(*xp && strncasecmp(xp, vname, vlen)) xp++;    // step through looking for our variable
+                if(*xp && !isnamechar(xp[vlen]))                    // is it terminated correctly?
+                    t = 0;                                          // yes, found the matching NEXT
+                else
+                    t--;                                            // no luck, just decrement our stack counter
+            }
+            if(t == 0) {                                            // found the matching NEXT
+                forstack[forindex].nextptr = p;                     // pointer to the start of the NEXT command
+                break;
+            }
+        }
+
+        // test the loop value at the start
+        if(forstack[forindex].vartype & T_INT)
+            test = (forstack[forindex].stepvalue.i >= 0 && *(MMINTEGER *)vptr > forstack[forindex].tovalue.i) || (forstack[forindex].stepvalue.i < 0 && *(MMINTEGER *)vptr < forstack[forindex].tovalue.i) ;
+        else
+            test = (forstack[forindex].stepvalue.f >= 0 && *(MMFLOAT *)vptr > forstack[forindex].tovalue.f) || (forstack[forindex].stepvalue.f < 0 && *(MMFLOAT *)vptr < forstack[forindex].tovalue.f) ;
+
+        if(test) {
+            // loop is invalid at the start, so go to the end of the NEXT command
+            skipelement(p);                                         // find the command after the NEXT command
+            nextstmt = p;                                           // this is where we will continue
+        } else {
+            forindex++;                                             // save the loop data and continue on with the command after the FOR statement
+        }
+    }
+}
