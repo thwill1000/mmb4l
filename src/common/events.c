6b3424da4ab4a9feed40d9567c422b87082d20aa
Thomas Hugo Williams <thomas.hugo.williams@gmail.com>
2025-04-13 11:06:31 -0400

    fix: set 'dirty' flag for 'window surface' when SDL_WINDOWEVENT_SHOWN
    Fixes problem on R36S where for some games the display was not
    being updated until the first button press caused a graphics
    operation that made the surface dirty.

--- src/common/events.c
+++ src/common/events.c
@@ -131,7 +131,8 @@ void events_pump() {
                         break;
                     }
 
-                    case SDL_WINDOWEVENT_EXPOSED: {
+                    case SDL_WINDOWEVENT_EXPOSED:
+                    case SDL_WINDOWEVENT_SHOWN: {
                         MmSurfaceId window_id = graphics_find_window(event.window.windowID);
                         if (window_id == -1) ON_FAILURE_ERROR(kInternalFault);
                         graphics_surfaces[window_id].dirty = true;
