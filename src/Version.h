dd516c6811b698243a18d67fe6912d9abcd30b7d
Thomas Hugo Williams <thomas.hugo.williams@gmail.com>
2025-08-01 14:07:38 +0100

    mmb4a: set version to v1.0-alpha.1

--- src/Version.h
+++ src/Version.h
@@ -48,7 +48,8 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #if defined(__linux__)
     #define __mmb4l__
     #if defined(__ANDROID__)
-        #define MM_ARCH  "Android aarch64"
+        #define MM_ARCH  "Android"
+//        #define MM_ARCH  "Android aarch64"
         #define ENV64BIT
     #elif defined(__x86_64)
         #define MM_ARCH  "Linux x86_64"
@@ -75,10 +76,17 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #if defined(__mmb4l__)
     #define MM_DEVICE     "MMB4L"
+#if defined(__ANDROID__)
+    #define MM_MAJOR      1
+    #define MM_MINOR      0
+    #define MM_MICRO      1
+    #define BUILD_NUMBER  0  // Currently always 0.
+#else
     #define MM_MAJOR      0
     #define MM_MINOR      8
     #define MM_MICRO      1
     #define BUILD_NUMBER  0  // Currently always 0.
+#endif
     #define MM_VERSION    (MM_MAJOR * 1000000000) + (MM_MINOR * 10000000) + (MM_MICRO) * 10000 + BUILD_NUMBER
     #define COPYRIGHT     "Copyright 2011-2025 Geoff Graham\r\n" \
                           "Copyright 2016-2025 Peter Mather\r\n" \
