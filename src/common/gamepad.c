00942524f5611464ad0ef6bb2210ae27d9985a36
Thomas Hugo Williams <thomas.hugo.williams@gmail.com>
2025-09-20 17:18:20 +0100

    refactor: eliminate 'mmmath.h'

--- src/common/gamepad.c
+++ src/common/gamepad.c
@@ -42,6 +42,8 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 *******************************************************************************/
 
+#include <math.h>
+
 #include "../Configuration.h"
 #include "cstring.h"
 #include "error.h"
