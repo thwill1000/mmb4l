ee23e5dbaa066e168b9ba10dc3354d0879be74c1
Thomas Hugo Williams <thomas.hugo.williams@gmail.com>
2025-09-20 17:00:41 +0100

    refactor: eliminate 'Hardware_Includes.h'

--- src/functions/fun_atan2.c
+++ src/functions/fun_atan2.c
@@ -44,7 +44,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <math.h>
 
-#include "../Hardware_Includes.h"
 #include "../common/mmb4l.h"
 #include "../core/Functions.h"
 
