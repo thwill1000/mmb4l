3e342fe1696351843bdf22f3c949872b187e693b
Thomas Hugo Williams <thomas.hugo.williams@gmail.com>
2025-10-13 20:47:11 +0100

    refactor: return MmResult from console_show_cursor()

--- src/common/gtest/stubs/console_stubs.c
+++ src/common/gtest/stubs/console_stubs.c
@@ -33,7 +33,7 @@ void console_set_cursor_char_pos(int x, int y) { }
 void console_set_cursor_pixel_pos(int x, int y) { }
 int console_set_size(int width, int height) { return -1; }
 void console_set_title(const char *title, bool command) { }
-void console_show_cursor(bool show) { }
+MmResult console_show_cursor(bool show) { return kOk; }
 MmResult console_underline(bool underline) { return kOk; }
 size_t console_write(const char *buf, size_t sz) { return 0; }
 void console_put_keypress(char ch) { }
