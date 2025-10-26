c6f3c1af9515e79cab93a5a0fa303f1c44862fa4
Thomas Hugo Williams <thomas.hugo.williams@gmail.com>
2025-10-26 09:22:41 +0100

    style: remove unused #include

--- src/common/file.c
+++ src/common/file.c
@@ -47,7 +47,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <errno.h>
 #include <fnmatch.h>
 #include <libgen.h>
-#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
