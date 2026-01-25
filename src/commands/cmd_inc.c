6a2e71752449e2a3ec868173cec53d1d18718579
Thomas Hugo Williams <thomas.hugo.williams@gmail.com>
2026-01-25 09:34:23 +0000

    refactor: tidy up cmd_inc()

--- src/commands/cmd_inc.c
+++ src/commands/cmd_inc.c
@@ -4,7 +4,7 @@ MMBasic for Linux (MMB4L)
 
 cmd_inc.c
 
-Copyright 2021-2025 Geoff Graham, Peter Mather and Thomas Hugo Williams.
+Copyright 2021-2026 Geoff Graham, Peter Mather and Thomas Hugo Williams.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
@@ -46,33 +46,35 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "../common/error.h"
 
 void cmd_inc(void) {
-    char *p, *q;
-    int vtype;
     getargs(&cmdline, 3, DELIM_COMMA);
+
+    char *p = findvar(argv[0], V_FIND);
+    if (vartbl[VarIndex].type & T_CONST) {
+        ERROR_CANNOT_CHANGE_A_CONSTANT;
+    }
+    const int vtype = TypeMask(vartbl[VarIndex].type);
+
     if (argc == 1) {
-        p = findvar(argv[0], V_FIND);
-        if (vartbl[VarIndex].type & T_CONST) ERROR_CANNOT_CHANGE_A_CONSTANT;
-        vtype = TypeMask(vartbl[VarIndex].type);
-        if (vtype & T_STR) ERROR_INVALID_VARIABLE;
-        if (vtype & T_NBR)
-            (*(MMFLOAT *)p) = (*(MMFLOAT *)p) + 1.0;
-        else if (vtype & T_INT)
-            *(long long int *)p = *(long long int *)p + 1;
-        else
-            ERROR_SYNTAX;
-    } else {
-        p = findvar(argv[0], V_FIND);
-        if (vartbl[VarIndex].type & T_CONST) ERROR_CANNOT_CHANGE_A_CONSTANT;
-        vtype = TypeMask(vartbl[VarIndex].type);
         if (vtype & T_STR) {
-            q = getstring(argv[2]);
-            if (*p + *q > MAXSTRLEN) ERROR_STRING_TOO_LONG;
+            ERROR_INVALID_VARIABLE;
+        } else if (vtype & T_NBR) {
+            *(MMFLOAT *)p = *(MMFLOAT *)p + 1.0;
+        } else if (vtype & T_INT) {
+            *(MMINTEGER *)p = *(MMINTEGER *)p + 1;
+        } else {
+            ON_FAILURE_ERROR(kSyntax);
+        }
+    } else {
+        if (vtype & T_STR) {
+            char *q = getstring(argv[2]);
+            if (*p + *q > MAXSTRLEN) ON_FAILURE_ERROR(kStringTooLong);
             Mstrcat(p, q);
         } else if (vtype & T_NBR) {
-            (*(MMFLOAT *)p) = (*(MMFLOAT *)p) + getnumber(argv[2]);
+            *(MMFLOAT *)p = *(MMFLOAT *)p + getnumber(argv[2]);
         } else if (vtype & T_INT) {
-            *(long long int *)p = *(long long int *)p + getinteger(argv[2]);
-        } else
-            ERROR_SYNTAX;
+            *(MMINTEGER *)p = *(MMINTEGER *)p + getinteger(argv[2]);
+        } else {
+            ON_FAILURE_ERROR(kSyntax);
+        }
     }
 }
