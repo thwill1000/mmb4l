dcd60df035067e112e69a508654f467435dd8dc3
Thomas Hugo Williams <thomas.hugo.williams@gmail.com>
2026-03-02 18:17:59 +0000

    chore: update (c) date in splash

--- src/Version.h
+++ src/Version.h
@@ -88,9 +88,9 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
     #define BUILD_NUMBER  0  // Currently always 0.
 #endif
     #define MM_VERSION    (MM_MAJOR * 1000000000) + (MM_MINOR * 10000000) + (MM_MICRO) * 10000 + BUILD_NUMBER
-    #define COPYRIGHT     "Copyright 2011-2025 Geoff Graham\r\n" \
-                          "Copyright 2016-2025 Peter Mather\r\n" \
-                          "Copyright 2021-2025 Thomas Hugo Williams"
+    #define COPYRIGHT     "Copyright 2011-2026 Geoff Graham\r\n" \
+                          "Copyright 2016-2026 Peter Mather\r\n" \
+                          "Copyright 2021-2026 Thomas Hugo Williams"
 #else
     #error __mmb4l__ is not defined
 #endif
