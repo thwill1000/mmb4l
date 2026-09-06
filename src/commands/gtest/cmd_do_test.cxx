e216d0ae406ef10e34b72c322fe4962b8bfb70ad
Thomas Hugo Williams <thomas.hugo.williams@gmail.com>
2026-09-06 18:33:15 +0100

    refactor: remove now redundant test stubs for keybuf_pump_tty()

--- src/commands/gtest/cmd_do_test.cxx
+++ src/commands/gtest/cmd_do_test.cxx
@@ -41,9 +41,6 @@ extern char cmd_run_args[STRINGSIZE];
 // Defined in "commands/cmd_run.c"
 MmResult cmd_run_parse_args(const char *p, char *filename, char *run_args);
 
-// Defined in "common/keybuf.c"
-void keybuf_pump_tty(void) { }
-
 // Defined in "common/events.c"
 void events_pump() { }
 
--- src/common/gtest/stubs/keybuf_stubs.c
+++ src/common/gtest/stubs/keybuf_stubs.c
@@ -9,6 +9,5 @@ int keybuf_count(void) { return 0; }
 void keybuf_clear(void) { }
 int keybuf_get(void) { return -1; }
 bool keybuf_isatty(void) { return true; }
-void keybuf_pump_tty(void) { }
 void keybuf_put(char ch) { }
 bool keybuf_exhausted() { return false; }
